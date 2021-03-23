9a6c9658b8a85b5c3b0f11512754e9d03e8b7a5a
yyyyyyyy-y <1974288772@qq.com>
2021-03-23 19:39:04 +0800

    [zly]接口错误拦截和环境配置

---
+++ src/env.js
@@ -0,0 +1,27 @@
+let baseURL
+// process.env.NODE_ENV获取node.js进程里的环境变量
+switch (process.env.NODE_ENV) {
+  // 开发
+  case 'development':
+    baseURL = 'http://dev-mall-pre.springboot.cn/api'
+    break
+  // 测试
+  case 'test':
+    baseURL = 'http://test-mall-pre.springboot.cn/api'
+    break
+  // 预发布
+  case 'prev':
+    baseURL = 'http://prev-mall-pre.springboot.cn/api'
+    break
+  // 线上
+  case 'prod':
+    baseURL = 'http://mall-pre.springboot.cn/api'
+    break
+  default:
+    baseURL = 'http://mall-pre.springboot.cn/api'
+    break
+}
+
+export default {
+  baseURL
+}
--- src/main.js
+++ src/main.js
@@ -3,8 +3,33 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import env from './env'
 
-Vue.config.productionTip = false
+Vue.use(VueAxios, axios)// 把axios对象挂载上去
+// 只使用axios时，每个页面都得导入，很麻烦，而vue-axios把作用域对象挂载到
+// vue实例上面去，方便用this调用
+// 根据前端跨域做调整，我们先暂时采取代理的方式
+axios.defaults.baseURL = '/api'
+axios.defaults.timeout = 6000
+// 接口错误拦截
+axios.interceptors.response.use(function (response) {
+  const res = response.data // 获取接口的整个信息（包括status，data，msg）
+  if (res.status === 0) { // 登陆成功的状态码为0
+    return res.data // 获取接口信息的data
+  } else if (res.status === 10) { // 未登录的状态码为10（自己设定）
+    window.location.href = '/#/login' // 直接跳转页面到login登陆页面，
+    // 不能用路由的方式，因为这是main.js文件不是vue
+  } else {
+    alert(res.msg) //  否则报错，输出错误msg信息
+  }
+})
+
+// 根据环境变量获取不同的请求地址
+axios.defaults.baseURL = env.baseURL
+
+Vue.config.productionTip = false// 生产环境的提示
 
 new Vue({
   router,
